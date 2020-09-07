244b4d46d08001a30c98770a10740aacac41a538
JonasSchool <j_s310304>
2020-09-07 09:14:18 +0200

    started to recode BanCommand.java

--- src/main/java/de/netdown/bungeesystem/commands/BanCommand.java
+++ src/main/java/de/netdown/bungeesystem/commands/BanCommand.java
@@ -10,6 +10,7 @@ import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
+import javax.swing.*;
 import java.util.UUID;
 
 public class BanCommand extends Command {
@@ -24,151 +25,74 @@ public class BanCommand extends Command {
 
     @Override
     public void execute(CommandSender commandSender, String[] args) {
-        if (commandSender instanceof ProxiedPlayer) {
-            ProxiedPlayer player = (ProxiedPlayer) commandSender;
-            if (player.hasPermission("bungee.ban.all") || player.hasPermission("bungee.ban")) {
-                if (args.length == 2) {
-                    try {
-                        BanManager manager = plugin.getBanManager();
-                        Reason reason = manager.getReasonTemplate(Integer.parseInt(args[1]));
-                        if (!reason.isCanSupBan() || player.hasPermission("bungee.ban.all")) {
-                            ProxiedPlayer target = ProxyServer.getInstance().getPlayer(args[0]);
-                            if (target != null) {
-                                if (reason.getReasonType() == Reason.ReasonType.BAN) {
-                                    if (manager.isBanned(target.getUniqueId()))
-                                        manager.unBan(target.getUniqueId());
-                                    manager.banPlayerByPlayer(target, player, reason);
-                                } else {
-                                    if (manager.isMuted(target.getUniqueId()))
-                                        manager.unMute(target.getUniqueId());
-                                    manager.mutePlayerByPlayer(target, player, reason);
-                                }
-                                int[] time = manager.secondsToArray(reason.getSeconds());
-                                for (ProxiedPlayer a : ProxyServer.getInstance().getPlayers()) {
-                                    if (a.hasPermission("bungee.ban")) {
-                                        if (reason.getReasonType() == Reason.ReasonType.BAN)
-                                            a.sendMessage(new TextComponent(plugin.getData().getPrefix() + "Der Spieler §b" + target.getName() + " §7wurde gebannt."));
-                                        else
-                                            a.sendMessage(new TextComponent(plugin.getData().getPrefix() + "Der Spieler §b" + target.getName() + " §7wurde gemutet."));
-                                        a.sendMessage(new TextComponent(plugin.getData().getPrefix() + "➥ §7Grund §8» §b" + reason.getReason()));
-                                        a.sendMessage(new TextComponent(plugin.getData().getPrefix() + "➥ §7Länge §8» " + manager.getTimeAsString(time)));
-                                        a.sendMessage(new TextComponent(plugin.getData().getPrefix() + "➥ §7Von §8» §b" + player.getName()));
-                                    }
-                                }
-                            } else {
-                                UUID tUUID = UUIDFetcher.getUUID(args[0]);
-                                if (tUUID == null) {
-                                    player.sendMessage(new TextComponent(plugin.getData().getPrefix() + "Der Spieler §b" + args[0] + " §7existiert nicht."));
-                                    return;
-                                }
-                                if (reason.getReasonType() == Reason.ReasonType.BAN) {
-                                    if (manager.isBanned(tUUID))
-                                        manager.unBan(tUUID);
-                                    manager.banOfflinePlayerByPlayer(args[0], player, reason);
-                                } else {
-                                    if (manager.isMuted(tUUID))
-                                        manager.unMute(tUUID);
-                                    manager.muteOfflinePlayerByPlayer(args[0], player, reason);
-                                }
-                                int[] time = manager.secondsToArray(reason.getSeconds());
-                                for (ProxiedPlayer a : ProxyServer.getInstance().getPlayers()) {
-                                    if (a.hasPermission("bungee.ban")) {
-                                        if (reason.getReasonType() == Reason.ReasonType.BAN)
-                                            a.sendMessage(new TextComponent(plugin.getData().getPrefix() + "Der Spieler §b" + args[0] + " §7wurde gebannt."));
-                                        else
-                                            a.sendMessage(new TextComponent(plugin.getData().getPrefix() + "Der Spieler §b" + args[0] + " §7wurde gemutet."));
-                                        a.sendMessage(new TextComponent(plugin.getData().getPrefix() + "➥ §7Grund §8» §b" + reason.getReason()));
-                                        a.sendMessage(new TextComponent(plugin.getData().getPrefix() + "➥ §7Länge §8» " + manager.getTimeAsString(time)));
-                                        a.sendMessage(new TextComponent(plugin.getData().getPrefix() + "➥ §7Von §8» §b" + player.getName()));
-                                    }
-                                }
-                            }
-                        } else
-                            player.sendMessage(new TextComponent(plugin.getData().getPrefix() + "Du hast keine Rechte, um einen Spieler diesen Gründen zu bannen."));
-                    } catch (NumberFormatException e) {
-                        sendHelp(player);
-                    }
-                } else
-                    sendHelp(player);
-            } else
-                player.sendMessage(new TextComponent(plugin.getData().getNoPerm()));
-        } else {
+        if (commandSender.hasPermission("bungee.ban") || commandSender.hasPermission("bungee.ban.all")) {
             if (args.length == 2) {
-                BanManager manager = plugin.getBanManager();
                 try {
-                    Reason reason = manager.getReasonTemplate(Integer.parseInt(args[1]));
-                    ProxiedPlayer target = ProxyServer.getInstance().getPlayer(args[0]);
-                    if (target != null) {
-                        if (reason.getReasonType() == Reason.ReasonType.BAN) {
-                            if (manager.isBanned(target.getUniqueId()))
-                                manager.unBan(target.getUniqueId());
-                            manager.banPlayerByConsole(target, reason);
-                        } else {
-                            if (manager.isMuted(target.getUniqueId()))
-                                manager.unMute(target.getUniqueId());
-                            manager.mutePlayerByConsole(target, reason);
-                        }
-                        int[] time = manager.secondsToArray(reason.getSeconds());
-                        for (ProxiedPlayer a : ProxyServer.getInstance().getPlayers()) {
-                            if (a.hasPermission("bungee.ban")) {
-                                if (reason.getReasonType() == Reason.ReasonType.BAN)
-                                    a.sendMessage(new TextComponent(plugin.getData().getPrefix() + "Der Spieler §b" + target.getName() + " §7wurde gebannt."));
+                    Reason reason = plugin.getBanManager().getReasonTemplate(Integer.parseInt(args[1]));
+                    if (reason == null) {
+                        commandSender.sendMessage(new TextComponent(plugin.getData().getPrefix() + "Diese ID ist nicht bekannt."));
+                        return;
+                    }
+                    UUID uuid = UUIDFetcher.getUUID(args[0]);
+                    if (uuid == null) {
+                        commandSender.sendMessage(new TextComponent(plugin.getData().getPrefix() + "Dieser Spieler existiert nicht."));
+                        return;
+                    }
+                    if (!reason.isCanSupBan()) {
+                        if (commandSender.hasPermission("bungee.ban.all")) {
+                            removeBan(reason, uuid);
+                            ProxiedPlayer player = plugin.getProxy().getPlayer(uuid);
+                            if (player == null) {
+                                if (commandSender instanceof ProxiedPlayer)
+                                    plugin.getBanManager().banOfflinePlayerByPlayer(args[0], (ProxiedPlayer) commandSender, reason);
                                 else
-                                    a.sendMessage(new TextComponent(plugin.getData().getPrefix() + "Der Spieler §b" + target.getName() + " §7wurde gemutet."));
-                                a.sendMessage(new TextComponent(plugin.getData().getPrefix() + "➥ §7Grund §8» §b" + reason.getReason()));
-                                a.sendMessage(new TextComponent(plugin.getData().getPrefix() + "➥ §7Länge §8» " + manager.getTimeAsString(time)));
-                                a.sendMessage(new TextComponent(plugin.getData().getPrefix() + "➥ §7Von §8» §bCONSOLE"));
-                            }
-                        }
-                    } else {
-                        UUID tUUID = UUIDFetcher.getUUID(args[0]);
-                        if (tUUID == null) {
-                            commandSender.sendMessage(new TextComponent(plugin.getData().getPrefix() + "Der Spieler §b" + args[0] + " §7existiert nicht."));
-                            return;
-                        }
-                        if (reason.getReasonType() == Reason.ReasonType.BAN) {
-                            if (manager.isBanned(tUUID))
-                                manager.unBan(tUUID);
-                            manager.banOfflinePlayerByConsole(args[0], reason);
-                        } else {
-                            if (manager.isMuted(tUUID))
-                                manager.unMute(tUUID);
-                            manager.muteOfflinePlayerByConsole(args[0], reason);
-                        }
-                        int[] time = manager.secondsToArray(reason.getSeconds());
-                        for (ProxiedPlayer a : ProxyServer.getInstance().getPlayers()) {
-                            if (a.hasPermission("bungee.ban")) {
-                                if (reason.getReasonType() == Reason.ReasonType.BAN)
-                                    a.sendMessage(new TextComponent(plugin.getData().getPrefix() + "Der Spieler §b" + args[0] + " §7wurde gebannt."));
+                                    plugin.getBanManager().banOfflinePlayerByConsole(args[0], reason);
+                            } else {
+                                if (commandSender instanceof ProxiedPlayer)
+                                    plugin.getBanManager().banPlayerByPlayer(player, (ProxiedPlayer) commandSender, reason);
                                 else
-                                    a.sendMessage(new TextComponent(plugin.getData().getPrefix() + "Der Spieler §b" + args[0] + " §7wurde gemutet."));
-                                a.sendMessage(new TextComponent(plugin.getData().getPrefix() + "➥ §7Grund §8» §b" + reason.getReason()));
-                                a.sendMessage(new TextComponent(plugin.getData().getPrefix() + "➥ §7Länge §8» " + manager.getTimeAsString(time)));
-                                a.sendMessage(new TextComponent(plugin.getData().getPrefix() + "➥ §7Von §8» §bCONSOLE"));
+                                    plugin.getBanManager().banPlayerByConsole(player, reason);
                             }
-                        }
+                            for(ProxiedPlayer all : plugin.getProxy().getPlayers())
+                                if(all.hasPermission("bungee.ban") || all.hasPermission("bungee.ban.all")) {
+
+                                }
+                        } else
+                            commandSender.sendMessage(new TextComponent(plugin.getData().getPrefix() + "Du darfst für diese ID nicht bannen."));
+                        return;
                     }
                 } catch (NumberFormatException e) {
-                    sendHelp(commandSender);
+                    commandSender.sendMessage(new TextComponent(plugin.getData().getPrefix() + "Bitte gebe als ID nur ganze Zahlen an."));
                 }
-            }else
+            } else
                 sendHelp(commandSender);
-        }
+        } else
+            commandSender.sendMessage(new TextComponent(plugin.getData().getNoPerm()));
     }
 
     private void sendHelp(CommandSender commandSender) {
         commandSender.sendMessage(new TextComponent(plugin.getData().getPrefix() + "Bitte benutze: /ban <Spieler> <ID>"));
         commandSender.sendMessage(new TextComponent(plugin.getData().getPrefix() + "Hier die Verfügbaren ID's"));
         for (Reason reason : plugin.getBanManager().getReasons()) {
-            if (!reason.isCanSupBan() || commandSender.hasPermission("bungee.ban.all"))
+            if (!reason.isCanSupBan() && commandSender.hasPermission("bungee.ban.all"))
                 if (reason.getReasonType() == Reason.ReasonType.BAN)
                     commandSender.sendMessage(new TextComponent(plugin.getData().getPrefix() + "➥ §b" + reason.getId() + " §8» §a" + reason.getReason()));
         }
         commandSender.sendMessage(new TextComponent(plugin.getData().getPrefix() + "Mutes"));
         for (Reason reason : plugin.getBanManager().getReasons()) {
-            if (!reason.isCanSupBan() || commandSender.hasPermission("bungee.ban.all"))
+            if (!reason.isCanSupBan() && commandSender.hasPermission("bungee.ban.all"))
                 if (reason.getReasonType() == Reason.ReasonType.MUTE)
                     commandSender.sendMessage(new TextComponent(plugin.getData().getPrefix() + "➥ §b" + reason.getId() + " §8» §a" + reason.getReason()));
+        }
+    }
+
+    private void removeBan(Reason reason, UUID uuid) {
+        if (reason.getReasonType() == Reason.ReasonType.BAN) {
+            if (plugin.getBanManager().isBanned(uuid))
+                plugin.getBanManager().unBan(uuid);
+        } else {
+            if (plugin.getBanManager().isMuted(uuid))
+                plugin.getBanManager().unMute(uuid);
         }
     }
 
--- src/main/java/de/netdown/bungeesystem/utils/MaintenanceManager.java
+++ src/main/java/de/netdown/bungeesystem/utils/MaintenanceManager.java
@@ -8,11 +8,41 @@ public class MaintenanceManager {
 
     private boolean maintenance;
     private boolean motdSupport;
+    private boolean protocolSupport;
 
     private String motdLine1;
     private String motdLine2;
+    private String protocolLine;
 
     public MaintenanceManager(BungeeSystem plugin) {
         this.plugin = plugin;
+    }
+
+    public boolean isProtocolSupport() {
+        return protocolSupport;
+    }
+
+    public String getProtocolLine() {
+        return protocolLine;
+    }
+
+    public boolean isMotdSupport() {
+        return motdSupport;
+    }
+
+    public String getMotdLine1() {
+        return motdLine1;
+    }
+
+    public String getMotdLine2() {
+        return motdLine2;
+    }
+
+    public boolean isMaintenance() {
+        return maintenance;
+    }
+
+    public void setMaintenance(boolean maintenance) {
+        this.maintenance = maintenance;
     }
 }
